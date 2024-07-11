f15d158fd71629f9a488556e1482fc33a52ff268
Astral890 <enac7125@outlook.com>
2024-07-10 19:58:06 -0600

    Comienzo del desarrollo

---
+++ app/index.js
@@ -0,0 +1,16 @@
+//Meter el dirmane
+import path from 'path';
+import {fileURLToPath} from 'url';
+const _dirname = path.dirname(fileURLToPath(import.meta.url));
+
+import express from 'express';
+
+//Crear el servidor
+const server=express();
+server.set("port",4000);
+server.listen(server.get("port"));
+server.get("/",(req,res)=> res.sendFile(_dirname + "/pages/mainpre.html"))
+server.get("/sUp",(req,res)=> res.sendFile(_dirname + "/pages/registroUsuario.html"))
+
+//Configuracion
+server.use(express.static(_dirname+"/public"))
